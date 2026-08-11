3d79c2370c6538f22a49c62cd83341bf465768ac
marvy896 <onyex896@gmail.com>
2026-08-11 21:28:09 +0100

    first project

---
+++ diceGame.py
@@ -0,0 +1,29 @@
+def roll_dice():
+    import random
+    return random.randint(1, 6)
+
+Name = input("Enter your name: ")
+print(f"Hello, {Name}! Welcome to my Dice Game!")
+def PlayGame():
+    print("Do you want to play? (y/n)")
+    while True:
+        choice = input().lower()
+        if choice == "y":
+            print("Great! Let's start the game.")
+            while True:
+                    input("Press Enter to roll the dice...")
+                    result = roll_dice()
+                    print(f"You rolled a {result}!")
+                    if result == 6:
+                        print("Congratulations! You rolled a 6! \n Press Enter to roll the dice..")
+                        break
+                    else:
+                        print("Try again!")
+        elif choice == "n":
+            print("Okay, maybe next time. Goodbye!")
+            break        
+        else:
+         print("Invalid input. Please enter 'y' or 'n'.")
+         
+PlayGame()
+
